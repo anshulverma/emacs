bb5cbbfbdee903b708a57acbb39488f586521e40
Anshul Verma <ansverma@adobe.com>
2014-05-23 16:28:50 -0700

    adding some more customizations

--- init.el
+++ init.el
@@ -1,5 +1,16 @@
 (add-to-list 'load-path "~/emacs/lisp/")
 
+(require 'emacs-type)
+
 (load-library "packages") ;; make sure all the packages are installed
 (load-library "custom") ;; customizations to the my emacs environment
 (load-library "modes") ;; mode and configurations
+
+ ;; startup "script" for when we've got a window system
+(if-not-terminal
+ (progn ;; start gnuserv, so apps can talk to us (e.g. p4, browsers)
+ (when (or google
+           (and winnt (not cygwin)))
+ (require 'gnuserv)
+ (setq gnuserv-frame (selected-frame))
+ (gnuserv-start))
--- lisp/custom.el
+++ lisp/custom.el
@@ -20,3 +20,28 @@
 (defvaralias 'c-basic-offset 'tab-width)
 (defvaralias 'cperl-indent-level 'tab-width)
 (setq-default indent-tabs-mode nil)
+
+(set-mouse-color "black")
+
+;; position and size window automatically based on display resolution
+(defun set-frame-size-according-to-resolution ()
+  (interactive)
+  (if window-system
+  (progn
+    ;; use 120 char wide window for largeish displays
+    ;; and smaller 80 column windows for smaller displays
+    ;; pick whatever numbers make sense for you
+    (if (> (x-display-pixel-width) 1280)
+           (add-to-list 'default-frame-alist (cons 'width 120))
+           (add-to-list 'default-frame-alist (cons 'width 80)))
+    ;; for the height, subtract a couple hundred pixels
+    ;; from the screen height (for panels, menubars and
+    ;; whatnot), then divide by the height of a char to
+    ;; get the height we want
+    (add-to-list 'default-frame-alist 
+         (cons 'height (/ (- (x-display-pixel-height) 200)
+                             (frame-char-height)))))))
+(set-frame-size-according-to-resolution)
+
+;; run shell in emacs
+(shell)
