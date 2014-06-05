8017fa95bf0a95169bb17d047c0946f86561773b
Anshul Verma <ansverma@adobe.com>
2014-06-05 14:15:22 -0700

    C-k C-k : delete current buffer file

--- lisp/key_bindings.el
+++ lisp/key_bindings.el
@@ -151,3 +151,18 @@
 
 ;; magit status buffer
 (global-set-key (kbd "C-c g") 'magit-status)
+
+;; delete file
+(defun delete-current-buffer-file ()
+  "Removes file connected to current buffer and kills buffer."
+  (interactive)
+  (let ((filename (buffer-file-name))
+        (buffer (current-buffer))
+        (name (buffer-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (ido-kill-buffer)
+      (when (yes-or-no-p "Are you sure you want to remove this file? ")
+        (delete-file filename)
+        (kill-buffer buffer)
+        (message "File '%s' successfully removed" filename)))))
+(global-set-key (kbd "C-x C-k") 'delete-current-buffer-file)
