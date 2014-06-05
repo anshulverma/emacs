6d433ce42efc51bc222bce374b2365906746ef6b
Anshul Verma <ansverma@adobe.com>
2014-06-05 14:58:02 -0700

    magit should run in fullscreen mode (q to exit)

--- lisp/modes.el
+++ lisp/modes.el
@@ -117,3 +117,17 @@
 
 ;; ----DELETE SELECTION----
 (delete-selection-mode)
+
+
+;; ----MAGIT----
+; run magit in full screen and restore windows when user presses q
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
