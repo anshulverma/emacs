57511eb0d447612bd21c34be2293f2cddc9c2751
Anshul Verma <ansverma@adobe.com>
2014-06-05 14:53:28 -0700

    auto refresh dired buffers

--- lisp/custom.el
+++ lisp/custom.el
@@ -189,3 +189,10 @@
 (require 'dired-details)
 (setq-default dired-details-hidden-string "--- ")
 (dired-details-install)
+
+;; Auto refresh buffers
+(global-auto-revert-mode 1)
+
+;; Also auto refresh dired, but be quiet about it
+(setq global-auto-revert-non-file-buffers t)
+(setq auto-revert-verbose nil)
