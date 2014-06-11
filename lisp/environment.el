5f468f729e8dafc1a4ca8ab93a072938aeb21aea
Anshul Verma <ansverma@adobe.com>
2014-06-10 23:24:36 -0700

    make COMMAND work as CTRL in OSX

--- lisp/environment.el
+++ lisp/environment.el
@@ -1,4 +1,8 @@
-;; ispell is hard to find in emacs
+;;; ----MAC OSX----
 (if (eq system-type 'darwin)
+
+    ;; ispell is hard to find in emacs
     (setq ispell-program-name "/usr/local/bin/ispell")
-    )
+
+    ;; make COMMAND key function as CTRL
+    (setq mac-command-modifier 'control))
