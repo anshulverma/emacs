3c060803e957dc085d789337e6b92973fb16fde7
Anshul Verma <ansverma@adobe.com>
2014-06-12 14:06:28 -0700

    C-(n/p) : move around in auto complete popup

--- lisp/key_bindings.el
+++ lisp/key_bindings.el
@@ -277,3 +277,5 @@ M-x compile.
 
 ; auto-complete binding
 (global-set-key [C-tab] 'ac-start)
+(define-key ac-complete-mode-map "\C-n" 'ac-next)
+(define-key ac-complete-mode-map "\C-p" 'ac-previous)
