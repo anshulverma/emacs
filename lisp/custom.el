1ba14140b9bb4f9d59977ea1fe31c3b2137892f5
Anshul Verma <anshul.verma86@gmail.com>
2014-06-18 11:37:28 -0700

    fix line number face for better visibility

--- lisp/custom.el
+++ lisp/custom.el
@@ -8,7 +8,7 @@
  '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil
                          :overline nil :underline nil :slant normal :weight normal :height 128
                          :width normal :foundry "unknown" :family "Courier New"))))
- '(linum ((t (:background "dim gray" :forground "gold" :family "Courier New"))))
+ '(linum ((t (:background "#bbbbbb" :foreground "#555555" :family "Courier New"))))
  '(popup-face ((t (:background "dim gray" :foreground "white smoke"))))
  '(mode-line ((t (:foreground "#030303" :background "#fdfdfd" :box nil))))
  '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil))))
