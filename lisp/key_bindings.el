59241226fad7971d364fddd7436c20d6040356bd
Anshul Verma <anshul.verma86@gmail.com>
2014-07-15 14:42:19 -0700

    <backtab> : un indent by 2 spaces

--- lisp/key_bindings.el
+++ lisp/key_bindings.el
@@ -286,3 +286,18 @@ M-x compile.
 
 ; iedit mode
 (global-set-key (kbd "C-c ;") 'iedit-mode)
+
+; unindent by 2 spaces
+(global-set-key (kbd "<backtab>") 'un-indent)
+(defun un-indent ()
+  "remove 2 spaces from beginning of the line"
+  (interactive)
+  (save-excursion
+    (save-match-data
+      (beginning-of-line)
+      (when (looking-at "^\\s-+")
+        (untabify (match-beginning 0) (match-end 0)))
+      (when (looking-at "^  ")
+        (replace-match "")))))
+
+;;; key_bindings.el ends here
