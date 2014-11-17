8a2939bcb0da4fc4e5183771da2a25e26d4e2f35
Anshul Verma <anshul.verma86@gmail.com>
2014-11-17 14:14:25 -0800

    fixing key layout of multiple cursor mode

--- lisp/modes.el
+++ lisp/modes.el
@@ -354,7 +354,7 @@
 
 ;; ----MULTIPLE CURSORS----
 (require 'multiple-cursors)
-(global-set-key (kbd "C-c .") 'mc/edit-lines)  ; cursor on each line in selection
+(global-set-key (kbd "C-c C-.") 'mc/edit-lines)  ; cursor on each line in selection
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c ,") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-c C-,") 'mc/mark-all-like-this)
