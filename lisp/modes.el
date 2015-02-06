22ab5658357e1699c7680ae14bcf5437ea4f5826
Anshul Verma <anshul.verma86@gmail.com>
2015-02-06 09:17:58 -0800

    moving web-mode config into its own file

--- lisp/modes.el
+++ lisp/modes.el
@@ -263,40 +263,7 @@
 
 
 ;; ----WEB-MODE----
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-
-(setq web-mode-engines-alist
-      '(("php"    . "\\.phtml\\'")
-        ("blade"  . "\\.blade\\."))
-      )
-
-(setq web-mode-markup-indent-offset 2)
-(setq web-mode-css-indent-offset 2)
-(setq web-mode-code-indent-offset 2)
-(setq web-mode-style-padding 1)
-(setq web-mode-script-padding 1)
-(setq web-mode-block-padding 0)
-(setq web-mode-comment-style 2)
-
-(setq web-mode-extra-snippets
-      '(("erb" . (("name" . ("beg" . "end"))))
-        ("php" . (("name" . ("beg" . "end"))
-                  ("name" . ("beg" . "end"))))
-        ))
-
-(setq web-mode-enable-css-colorization t)
-(setq web-mode-enable-block-face t)
-(setq web-mode-enable-part-face t)
-(setq web-mode-enable-comment-keywords t)
-(setq web-mode-enable-current-element-highlight t)
+(require 'setup-web-mode)
 
 
 ;; ----CSV----
@@ -364,3 +331,7 @@
 (autoload 'groovy-mode "groovy-mode" "Groovy editing mode." t)
 (add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
 (add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
+
+
+(provide 'modes)
+;;; modes.el ends here
