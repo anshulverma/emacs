a5d555632ce05c9023b198e1b1fcd77dbc1185b0
Anshul Verma <anshul.verma86@gmail.com>
2016-05-15 19:37:27 -0700

    add more global config

--- src/custom/02-global.el
+++ src/custom/02-global.el
@@ -8,6 +8,11 @@
 ;; turn on syntax highlighting
 (global-font-lock-mode 1)
 
+;; how long lines are handled.  This
+;; appears to wrap long lines visually,
+;; but not add line-returns
+(global-visual-line-mode 1)
+
 ;; Auto refresh buffers
 (global-auto-revert-mode 1)
 
@@ -63,6 +68,30 @@
 
 ;; diminish keeps the modeline tidy
 (require 'diminish)
+
+;; saner regex syntax
+(require 're-builder)
+(setq reb-re-syntax 'string)
+
+;; disable backup file creation
+(setq backup-inhibited t)
+
+;; answer with y/n instead of yes/no
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; http://emacsredux.com/blog/2013/04/05/recently-visited-files
+(require 'recentf)
+(setq recentf-max-saved-items 200
+      recentf-max-menu-items 15)
+(recentf-mode +1)
+(setq save-place-file
+      (expand-file-name "user/saved-places" av-basedir))
+(global-set-key (kbd "<f7>") 'helm-recentf)
+
+;; tell emacs where to read abbrev definitions from
+(setq abbrev-file-name (expand-file-name "abbrev_defs" av-basedir))
+(setq save-abbrevs t)
+(setq-default abbrev-mode t)
 
 (provide '02-global)
 ;;; 02-global.el ends here
