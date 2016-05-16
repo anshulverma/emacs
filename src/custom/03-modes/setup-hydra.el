205b7e9809af4783ae344d0cfd3863256a49c6d6
Anshul Verma <anshul.verma86@gmail.com>
2016-05-15 19:41:29 -0700

    add hydra setup

---
+++ src/custom/03-modes/setup-hydra.el
@@ -0,0 +1,12 @@
+;;; setup-hydra --- Summary
+;;; Commentary:
+;;; Code:
+
+(require 'hydra)
+(require 'url)
+(require 'xml)
+
+(setq hydra-is-helpful t)
+
+(provide 'setup-hydra)
+;;; setup-hydra.el ends here
