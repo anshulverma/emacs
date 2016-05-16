0145c6946d8c3b095ef9791f647ed7415fa3a66a
Anshul Verma <anshul.verma86@gmail.com>
2016-05-15 23:50:10 -0700

    ibuffer navigation should wrap around

--- src/custom/05-ibuffer.el
+++ src/custom/05-ibuffer.el
@@ -1,9 +1,9 @@
 ;;; 05-ibuffer --- Summary
 ;;; Commentary:
+
 ;;; Code:
 
-(global-set-key (kbd "C-x b") 'buffer-menu)
-
+(global-set-key (kbd "C-x b") 'ibuffer)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (autoload 'ibuffer "ibuffer" "List buffers." t)
 
@@ -28,6 +28,28 @@
               (mode 16 16 :left :elide)
               " "
               filename-and-process)))
+
+(defun ibuffer-previous-line ()
+  "Wrap around to last line when going to previous line from top."
+  (interactive) (previous-line)
+  (if (<= (line-number-at-pos) 2)
+      (goto-line (- (count-lines (point-min) (point-max)) 2))))
+(defun ibuffer-next-line ()
+  "Wrap around to first line when going to next line from last."
+  (interactive) (next-line)
+  (if (>= (line-number-at-pos) (- (count-lines (point-min) (point-max)) 1))
+      (goto-line 3)))
+(define-key ibuffer-mode-map (kbd "<up>") 'ibuffer-previous-line)
+(define-key ibuffer-mode-map (kbd "<down>") 'ibuffer-next-line)
+
+;; Switching to ibuffer puts the cursor on the most recent buffer
+(defadvice ibuffer (around ibuffer-point-to-most-recent) ()
+           "Open ibuffer with cursor pointed to most recent buffer name"
+           (let ((recent-buffer-name (buffer-name)))
+             ad-do-it
+             (ibuffer-jump-to-buffer recent-buffer-name)
+             (ibuffer-forward-line)))
+(ad-activate 'ibuffer)
 
 (provide '05-ibuffer)
 ;;; 05-ibuffer.el ends here
