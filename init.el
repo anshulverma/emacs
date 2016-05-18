2f22ebedc5d9ec62b05c3d58d2326a08cd681aa1
Anshul Verma <anshul.verma86@gmail.com>
2016-05-18 15:38:14 -0700

    fix style in init.el

--- init.el
+++ init.el
@@ -29,13 +29,9 @@
 (add-to-list 'load-path (expand-file-name "jmax" av-lib-dir))
 
 (add-to-list 'load-path av-src-dir)
-
 (require 'av-packages)
 (require 'av-setup)
 
-(provide 'init)
-
-;;; init.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -56,3 +52,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(provide 'init)
+
+;;; init.el ends here
