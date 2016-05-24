71495301637b27edc21550a99b8bbad8c6fdcf6e
Anshul Verma <anshul.verma86@gmail.com>
2016-05-23 23:41:55 -0700

    add setup for ido mode

--- src/custom/03-modes/setup-ido.el
+++ src/custom/03-modes/setup-ido.el
@@ -2,7 +2,37 @@
 ;;; Commentary:
 ;;; Code:
 
-(ido-vertical-mode)
+(ido-mode 1)
+(setq ido-use-faces nil
+      ido-enable-prefix nil
+      ido-enable-flex-matching t
+      ido-case-fold nil
+      ido-auto-merge-work-directories-length -1
+      ido-create-new-buffer 'always
+      ido-use-filename-at-point nil
+      ido-max-prospects 10)
+
+;; use flx-ido for better name matching
+(require 'flx-ido)
+(require 'ido-vertical-mode)
+(ido-everywhere 1)
+(flx-ido-mode 1)
+(ido-vertical-mode 1)
+
+;; Use ido everywhere
+(require 'ido-ubiquitous)
+(ido-ubiquitous-mode 1)
+
+;; Fix ido-ubiquitous for newer packages
+(defmacro ido-ubiquitous-use-new-completing-read (cmd package)
+  `(eval-after-load ,package
+     '(defadvice ,cmd (around ido-ubiquitous-new activate)
+        (let ((ido-ubiquitous-enable-compatibility nil))
+          ad-do-it))))
+
+(ido-ubiquitous-use-new-completing-read webjump 'webjump)
+(ido-ubiquitous-use-new-completing-read yas/expand 'yasnippet)
+(ido-ubiquitous-use-new-completing-read yas/visit-snippet-file 'yasnippet)
 
 (provide 'setup-ido)
 ;;; setup-ido.el ends here
