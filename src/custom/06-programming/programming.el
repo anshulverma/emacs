12f7bb693aa4a4fa91ded9cb4523d461372b627c
Anshul Verma <anshul.verma86@gmail.com>
2016-05-27 09:39:09 -0700

    define auto-indent function for java mode

--- src/custom/06-programming/programming.el
+++ src/custom/06-programming/programming.el
@@ -3,6 +3,7 @@
 ;;; Code:
 
 (defun custom-programming-modes-hook ()
+  "Define common configurations for common programming modes."
    (make-local-variable 'column-number-mode)
    (column-number-mode t)
    (if window-system (hl-line-mode t))
@@ -14,6 +15,27 @@
 
 ;; enable yas snippets in programming modes
 (add-hook 'prog-mode-hook #'yas-minor-mode)
+
+;; auto indent on opening brace
+(require 'cc-mode)
+(defun av/auto-indent-method ()
+  "Automatically indent a method by adding two newlines.
+Puts point in the middle line as well as indent it by correct amount."
+  (interactive)
+  (newline-and-indent)
+  (newline-and-indent)
+  (forward-line -1)
+  (c-indent-line-or-region))
+
+(defun av/auto-indent-method-maybe ()
+  "Check if point is at a closing brace then auto indent."
+  (interactive)
+  (let ((char-at-point (char-after (point))))
+    (if (char-equal ?} char-at-point)
+        (av/auto-indent-method)
+      (newline-and-indent))))
+
+(define-key java-mode-map (kbd "RET") 'av/auto-indent-method-maybe)
 
 (provide 'programming)
 ;;; programming.el ends here
