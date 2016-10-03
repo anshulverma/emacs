7c932842c8d068c8b5a4c540e462034a225f925b
Anshul Verma <anshul.verma86@gmail.com>
2016-10-03 07:54:14 -0700

    set up path properly if running in window

--- src/custom/02-environment.el
+++ src/custom/02-environment.el
@@ -23,5 +23,12 @@
       ;; set function key as hyper
       (setq ns-function-modifier 'hyper)))
 
+;; fix the PATH variable
+(defun set-exec-path-from-shell-PATH ()
+  (let ((path-from-shell (shell-command-to-string "TERM=vt100 $SHELL -i -c 'echo $PATH'")))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+(when window-system (set-exec-path-from-shell-PATH))
+
 (provide '02-environment)
 ;;; 02-environment.el ends here
