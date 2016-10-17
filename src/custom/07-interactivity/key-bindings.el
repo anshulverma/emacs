a1d64a34550ffe804f7c6be021e66e13d86f305f
Anshul Verma <anshul.verma86@gmail.com>
2016-10-16 20:42:57 -0700

    set key chord CC to open calendar

--- src/custom/07-interactivity/key-bindings.el
+++ src/custom/07-interactivity/key-bindings.el
@@ -11,6 +11,7 @@
 (key-chord-define-global "FF" 'find-file)
 (key-chord-define-global "JB" 'beginning-of-buffer)
 (key-chord-define-global "JE" 'end-of-buffer)
+(key-chord-define-global "CC" 'av/open-calendar)
 (key-chord-mode +1)
 
 (global-set-key (kbd "C-c d") 'duplicate-current-line)
