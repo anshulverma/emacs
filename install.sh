86d3f5060b6e3f8391f5944dd2903e7d13426dd9
Anshul Verma <anshul.verma86@gmail.com>
2016-10-30 22:07:03 -0700

    install pyenv version and provide helpful error

--- install.sh
+++ install.sh
@@ -2,6 +2,17 @@
 
 basedir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+case "$OSTYPE" in
+  darwin*)  ;;
+
+  *)        echo "Only MacOSX supported currently."
+            echo "Please consider adding support for your OS type."
+            exit 1
+            ;;
+esac
+
+# all the base packages are installed using homebrew
+
 brew update
 
 packages=("ispell"
@@ -21,6 +32,14 @@ packages=("ispell"
 for pkg in "${packages[@]}"; do
   brew install $pkg
 done
+
+# install python and required packages
+
+python_version="2.7.10"
+pyenv install $python_version
+pyenv local $python_version
+
+pip install --upgrade pip
 
 pip_packages=("jedi"
               "elpy"
