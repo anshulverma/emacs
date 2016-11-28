83e690339277d78c30071b1b86a29126428f07db
Anshul Verma <anshul.verma86@gmail.com>
2016-11-28 11:39:44 -0800

    add setup script for clojure mode

---
+++ src/custom/06-programming/clojure.el
@@ -0,0 +1,9 @@
+;;; clojure --- Summary
+;;; Commentary:
+;;; Code:
+
+(add-hook 'cider-repl-mode-hook #'company-mode)
+(add-hook 'cider-mode-hook #'company-mode)
+
+(provide 'clojure)
+;;; clojure.el ends here
