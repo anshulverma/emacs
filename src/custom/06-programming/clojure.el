8bb43c622cbd89aa6fd8dd347dd47fa17ea24fdd
Anshul Verma <anshul.verma86@gmail.com>
2016-12-09 21:56:26 -0800

    enable eldoc in cider mode

--- src/custom/06-programming/clojure.el
+++ src/custom/06-programming/clojure.el
@@ -2,7 +2,9 @@
 ;;; Commentary:
 ;;; Code:
 
+(add-hook 'cider-repl-mode-hook #'eldoc-mode)
 (add-hook 'cider-repl-mode-hook #'company-mode)
+(add-hook 'cider-mode-hook #'eldoc-mode)
 (add-hook 'cider-mode-hook #'company-mode)
 
 (provide 'clojure)
