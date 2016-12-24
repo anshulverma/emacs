7e19c9ffbc24ebdcdc1cf8187104a9143f317882
Anshul Verma <anshul.verma86@gmail.com>
2016-12-23 18:05:25 -0800

    set C-) for sp-up-sexp (jumps to end of sexp)

--- src/custom/03-modes/setup-smartparens.el
+++ src/custom/03-modes/setup-smartparens.el
@@ -9,11 +9,13 @@
 (show-smartparens-global-mode t)
 
 (global-set-key (kbd "C-c C-p C-n") 'sp-forward-sexp)
-(global-set-key (kbd "C-c C-p C-p") 'sp-forward-sexp)
+(global-set-key (kbd "C-c C-p C-p") 'sp-backward-sexp)
 (global-set-key (kbd "C-c C-p C-b") 'sp-backward-slurp-sexp)
 (global-set-key (kbd "C-c C-p C-f") 'sp-forward-slurp-sexp)
 (global-set-key (kbd "C-c C-p C-t") 'sp-transpose-sexp)
 (global-set-key (kbd "C-c C-p C-l") 'sp-splice-sexp)
+
+(global-set-key (kbd "C-)") 'sp-up-sexp)
 
 (provide 'setup-smartparens)
 ;;; setup-smartparens.el ends here
