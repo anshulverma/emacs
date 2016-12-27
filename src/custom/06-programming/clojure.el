7ce622ae425a5cc0680054b3042d0c9acdf6323b
Anshul Verma <anshul.verma86@gmail.com>
2016-12-27 01:15:12 -0800

    enable subword mode in cider

--- src/custom/06-programming/clojure.el
+++ src/custom/06-programming/clojure.el
@@ -5,15 +5,19 @@
 ;;; setup `cider'
 (add-hook 'cider-repl-mode-hook #'eldoc-mode)
 (add-hook 'cider-repl-mode-hook #'company-mode)
+(add-hook 'cider-repl-mode-hook #'subword-mode)
 (add-hook 'cider-mode-hook #'eldoc-mode)
 (add-hook 'cider-mode-hook #'company-mode)
+(add-hook 'cider-mode-hook #'subword-mode)
 
 ;;; setup `clj-refactor'
 (require 'clj-refactor)
 
 (defun my-clojure-mode-hook ()
+  "Setup clojure mode settings."
+  (interactive)
   (clj-refactor-mode 1)
-  (yas-minor-mode 1) ; for adding require/use/import statements
+  (yas-minor-mode 1)
   ;; This choice of keybinding leaves cider-macroexpand-1 unbound
   (cljr-add-keybindings-with-prefix "C-c C-m"))
 
