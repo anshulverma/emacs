2d552415f2b088d47e9d0770edd9d44a02b5072a
Anshul Verma <anshul.verma86@gmail.com>
2017-01-03 01:30:55 -0800

    use brew to install fonts

--- install-font.sh
+++ install-font.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-basedir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cp $basedir/fonts/*.ttf $HOME/Library/Fonts/
+# basedir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# cp $basedir/fonts/*.ttf $HOME/Library/Fonts/
+
+brew update
+brew tap caskroom/fonts
+
+brew cask install font-inconsolata
+brew cask install font-symbola
+brew cask install font-source-code-pro
