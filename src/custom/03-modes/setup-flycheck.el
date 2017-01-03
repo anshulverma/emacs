4a790cf8ead2de15371c1440b66dbed41401f1ba
Anshul Verma <anshul.verma86@gmail.com>
2017-01-02 16:44:54 -0800

    add hydra for flycheck on "C-c ! !"

--- src/custom/03-modes/setup-flycheck.el
+++ src/custom/03-modes/setup-flycheck.el
@@ -8,5 +8,19 @@
 (eval-after-load 'flycheck
   '(flycheck-pos-tip-mode))
 
+(defhydra hydra-flycheck
+  (:pre (progn (setq hydra-lv t) (flycheck-list-errors))
+        :post (progn (setq hydra-lv nil) (quit-windows-on "*Flycheck errors*"))
+        :hint nil)
+  "Errors"
+  ("f"  flycheck-error-list-set-filter                            "Filter")
+  ("j"  flycheck-next-error                                       "Next")
+  ("k"  flycheck-previous-error                                   "Previous")
+  ("gg" flycheck-first-error                                      "First")
+  ("G"  (progn (goto-char (point-max)) (flycheck-previous-error)) "Last")
+  ("q"  nil))
+
+(global-set-key (kbd "C-c ! !") 'hydra-flycheck/body)
+
 (provide 'setup-flycheck)
 ;;; setup-flycheck.el ends here
